9d8755bc2e731096ac0f880ed4b010865cf3c589
priyansheeChouhan <pc480447@gmail.com>
2025-07-28 23:20:56 +0530

    working on Awards

--- Frontend/src/landing_pages/Home/Awards.jsx
+++ Frontend/src/landing_pages/Home/Awards.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Awards() {
     return (
-        <h1>Awards Section</h1>
+        <div className="container mt-5 py-4">
+            <div className="row">
+                <div className="col-6 px-5">
+                    <img src="images/largestBroker.svg" alt="" />
+                </div>
+                <div className="col-6 py-3 px-5">
+                    <h2>Largest stock broker in India</h2>
+                    <p className="fs-7 fw-semibold" >2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by 
+                        trading and investing in:</p>
+                    <div className="row mt-5 fw-semibold">
+                        <div className="col-6">
+                            <ul>
+                                <li>Futures and Options</li><br></br>
+                                <li>Commodity derivatives</li><br></br>
+                                <li>Currency derivatives</li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>Stocks & IPOs</li><br></br>
+                                <li>Direct mutual funds</li><br></br>
+                                <li>Bonds and Govt</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- Frontend/src/landing_pages/Home/Hero.jsx
+++ Frontend/src/landing_pages/Home/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Hero() {
     return (
-        <section className='text-center px-5'>
-            <div className="conatiner mt-7 align-items-center">
+        <section className='text-center px-5 pb-5'>
+            <div className="conatiner my-5 align-items-center">
             <div className='row'>
                 <img src="images/homeHero.png"
                   alt="Hero image"
@@ -12,7 +12,7 @@ function Hero() {
                 />
                 <h1 className='mt-2 fs-2'>Invest in everything</h1>
                 <p className='fs-5 mb-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='btn btn-primary btn-lg m-auto' style={{width: "25%"}}>Sign up for free</button>
+                <button className='btn btn-primary btn-lg m-auto' style={{width: "25%"}}>Sign up for free <i class="fa-solid fa-circle-arrow-right"></i></button>
             </div>
         </div>
         </section>
