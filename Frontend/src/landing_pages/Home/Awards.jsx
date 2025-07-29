f0e8ee16de6c71c26bb5efc7d0172084671d52b0
priyansheeChouhan <pc480447@gmail.com>
2025-07-29 22:40:33 +0530

    Awards component

--- Frontend/src/landing_pages/Home/Awards.jsx
+++ Frontend/src/landing_pages/Home/Awards.jsx
@@ -9,17 +9,17 @@ function Awards() {
                 </div>
                 <div className="col-6 py-3 px-5">
                     <h2>Largest stock broker in India</h2>
-                    <p className="fs-7 fw-semibold" >2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by 
+                    <p className="fs-7" >2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by 
                         trading and investing in:</p>
-                    <div className="row mt-5 fw-semibold">
-                        <div className="col-6">
+                    <div className="row mt-5">
+                        <div className="col-6 px-0">
                             <ul>
                                 <li>Futures and Options</li><br></br>
                                 <li>Commodity derivatives</li><br></br>
                                 <li>Currency derivatives</li>
                             </ul>
                         </div>
-                        <div className="col-6">
+                        <div className="col-6 px-0">
                             <ul>
                                 <li>Stocks & IPOs</li><br></br>
                                 <li>Direct mutual funds</li><br></br>
@@ -27,6 +27,11 @@ function Awards() {
                             </ul>
                         </div>
                     </div>
+                    <img 
+                    src="images/pressLogos.png" 
+                    alt="press logos" 
+                    className='mt-4'
+                    style={{ maxWidth: "80vh", width: "auto" }}/>
                 </div>
             </div>
         </div>
