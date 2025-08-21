97dc5cbe403fb64e159dc52e36cbda50950523fb
priyansheeChouhan <pc480447@gmail.com>
2025-08-21 20:31:02 +0530

    worked on support createTicket component

--- Frontend/src/landing_pages/Support/CreateTicket.jsx
+++ Frontend/src/landing_pages/Support/CreateTicket.jsx
@@ -81,6 +81,168 @@ function CreateTicket() {
               </div>
             </div>
           </div>
+          <div className="mb-4">
+            <div
+              className="border d-flex dropdown-div"
+              data-bs-toggle="collapse"
+              href="#multiCollapseExample3"
+              aria-expanded="false"
+              aria-controls="multiCollapseExample3">
+              <div className="bg-light d-flex align-items-center">
+                <i class="fa-brands fa-flutter mx-3 my-3"></i>
+              </div>
+              <div className="fw-semibold my-3 ms-3">Kite</div>
+              <div className="d-flex align-items-center flex-grow-1 justify-content-end">
+                <i class="fa-solid fa-angle-down mx-3 my-3"></i>
+              </div>
+            </div>
+            <div class="collapse multi-collapse" id="multiCollapseExample3">
+              <div class="card card-body">
+                <ul>
+                  <a href="#" className="text-decoration-none">
+                    <li>IPO</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Trading FAQs</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>
+                      Margin Trading Facility (MTF) and Margins
+                    </li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Charts and orders</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Alerts and Nudges</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>General</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="mb-4">
+            <div
+              className="border d-flex dropdown-div"
+              data-bs-toggle="collapse"
+              href="#multiCollapseExample4"
+              aria-expanded="false"
+              aria-controls="multiCollapseExample4">
+              <div className="bg-light d-flex align-items-center">
+                <i class="fa-solid fa-hand-holding-dollar mx-3 my-3"></i>
+              </div>
+              <div className="fw-semibold my-3 ms-3">Funds</div>
+              <div className="d-flex align-items-center flex-grow-1 justify-content-end">
+                <i class="fa-solid fa-angle-down mx-3 my-3"></i>
+              </div>
+            </div>
+            <div class="collapse multi-collapse" id="multiCollapseExample4">
+              <div class="card card-body">
+                <ul>
+                  <a href="#" className="text-decoration-none">
+                    <li>Add money</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Withdraw money</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>
+                      Add bank accounts
+                    </li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>eMandates</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="mb-4">
+            <div
+              className="border d-flex dropdown-div"
+              data-bs-toggle="collapse"
+              href="#multiCollapseExample5"
+              aria-expanded="false"
+              aria-controls="multiCollapseExample5">
+              <div className="bg-light d-flex align-items-center">
+                <i class="fa-solid fa-circle-notch mx-3 my-3"></i>
+              </div>
+              <div className="fw-semibold my-3 ms-3">Console</div>
+              <div className="d-flex align-items-center flex-grow-1 justify-content-end">
+                <i class="fa-solid fa-angle-down mx-3 my-3"></i>
+              </div>
+            </div>
+            <div class="collapse multi-collapse" id="multiCollapseExample5">
+              <div class="card card-body">
+                <ul>
+                  <a href="#" className="text-decoration-none">
+                    <li>Portfolio</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Corporate actions</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>
+                      Funds statement
+                    </li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Reports</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Profile</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Segments</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className="border d-flex dropdown-div"
+              data-bs-toggle="collapse"
+              href="#multiCollapseExample6"
+              aria-expanded="false"
+              aria-controls="multiCollapseExample6">
+              <div className="bg-light d-flex align-items-center">
+                <i class="fa-regular fa-circle mx-3 my-3"></i>
+              </div>
+              <div className="fw-semibold my-3 ms-3">Coin</div>
+              <div className="d-flex align-items-center flex-grow-1 justify-content-end">
+                <i class="fa-solid fa-angle-down mx-3 my-3"></i>
+              </div>
+            </div>
+            <div class="collapse multi-collapse" id="multiCollapseExample6">
+              <div class="card card-body">
+                <ul>
+                  <a href="#" className="text-decoration-none">
+                    <li>Mutual funds</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>National Pension Scheme (NPS)</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>
+                      Fixed Deposit (FD)
+                    </li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Features on Coin</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>Payments and Orders</li>
+                  </a>
+                  <a href="#" className="text-decoration-none">
+                    <li>General</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-lg-4"></div>
       </div>
@@ -89,3 +251,5 @@ function CreateTicket() {
 }
 
 export default CreateTicket;
+
+
