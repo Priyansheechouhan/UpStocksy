f4ff142aef0c3a861e6d185debd372812949d9d3
priyansheeChouhan <pc480447@gmail.com>
2025-08-27 20:35:48 +0530

    worked on components

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './Components/Home.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
